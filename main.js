4dc73720bcef5c7d3bb9721614481d93edff294d
Ryan Haining <haining.cpp@gmail.com>
2020-04-28 18:53:28 -0700

    fadein/fadeout effect, size tweaks.

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 let counter = 1;
 
 const QUESTIONS = [
-  'What is something your partner is worse at than they believe they are?',
-  'If you told your partner something in confidence, who is the first person that they would repeat it to?',
   'What is something that you do because your partner enjoys it, but you would never do otherwise?',
-  "Which of your partner's friends is the worst influence on them?",
+  'If you told your partner something in confidence, who is the first person that they would repeat it to?',
   'When in public together, what is something your partner does that embarrasses you?',
-  "What is something about your partner for which you find yourself apologizing to your friends or family?",
+  "Which of your partner's friends is the worst influence on them?",
   'Think of a word, phrase, or expression that your partner misuses.',
   'Describe your most positive memorable sexual experience.',
+  'What is something your partner is worse at than they believe they are?',
+  'How many of your coworkers would you describe as attractive?',
   "Of your partner's previous relationships, which one makes you feel the least secure?",
+  "If a crystal ball could show you the truth about any events in your partner's life for which you were not present, what would they be?",
   'In what ways does your partner fail to clean up after themself?',
   'Who are the top three people you sexually fantasize about the most often?',
   "If you could change any one thing about your partner's morals, what would it be?",
   "What aspects of your partner's family dynamic do you consider strange?",
-  'How many of your coworkers would you describe as attractive?',
   'Which of your platonic friends do you find the most attractive?',
-  "If a crystal ball could show you the truth about any events in your partner's life for which you were not present, what would they be?",
   "Is there something your partner has put off doing for a long time? Why haven't they done it yet?",
   'Make three "we" statements that you wish were true, but are not.',
+  "What is something about your partner for which you find yourself apologizing to your friends or family?",
   "Your partner's home catches fire. What do you hope would be destroyed? Why?",
 ];
   
@@ -63,12 +63,23 @@ function setQuestionText() {
   document.getElementById('question').innerText = QUESTIONS[counter-1];
 }
 
+function updateQuestionText() {
+  document.getElementById('question').style.animation = 'fadeout .2s linear';
+}
+
+
 function update() {
   setButtonVisibility();
   setCounterText();
-  setQuestionText();
+  updateQuestionText();
 }
 
 window.onload = function() {
+  const question = document.getElementById('question')
+  question.addEventListener('animationend',
+    function() {
+      setQuestionText();
+      question.style.animation = 'fadein .2s linear';
+    });
   update();
 }
